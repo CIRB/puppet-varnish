02e29d85e04469a316ca09a669188de9f17159c5
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-04-05 16:34:33 +0200

    FIX : path in file_line and require varnish::server

--- manifests/backend.pp
+++ manifests/backend.pp
@@ -8,18 +8,18 @@ define varnish::backend (
     if $probe != '' {
       file_line {
         "varnish_backend_$name$host$port":
-            file    => '/etc/varnish/backends.vcl',
+            path    => '/etc/varnish/backends.vcl',
             line    => template('varnish/backend.erb'),
             notify  => Service['varnish'],
-            require => File['/etc/varnish/backends.vcl']
+            require => [File['/etc/varnish/backends.vcl']]
       }
     } else {
       file_line {
         "varnish_backend_$name$host$port":
-            file    => '/etc/varnish/backends.vcl',
+            path    => '/etc/varnish/backends.vcl',
             line    => template('varnish/backend_simple.erb'),
             notify  => Service['varnish'],
-            require => File['/etc/varnish/backends.vcl']
+            require => [File['/etc/varnish/backends.vcl']]
       }
     }
 
