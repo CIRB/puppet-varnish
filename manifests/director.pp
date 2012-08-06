24f76e2e3d9fc911c08c74219d5382a1b9017b70
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-08-06 15:18:11 +0200

    update plone role and profiles

--- manifests/director.pp
+++ manifests/director.pp
@@ -4,6 +4,7 @@ define varnish::director ( $backends ) {
             ensure  => present,
             path    => '/etc/varnish/directors.vcl',
             line    => template('varnish/director.erb'),
+            notify  => Service['varnish'],
             require => File['/etc/varnish/directors.vcl']
         }
 
