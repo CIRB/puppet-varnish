1b454cf92a14ef6660b2bac5e029cd941225df6a
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-12-13 10:56:22 +0100

    add acetterbeek

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,30 @@
 class varnish::init {
 
-  yumrepo {
-    'varnish':
+  $version = hiera('varnish-version', 'installed')
+  if $version == 'installed' {
+    yumrepo {'varnish':
       descr    => 'varnish repository',
       baseurl  => 'http://repo.varnish-cache.org/redhat/el5/x86_64/',
       enabled  => 1,
       gpgcheck => 0,
-  }
+    }
 
-  package {
-    'varnish':
+    package {'varnish':
       ensure  => 'installed',
       require => Yumrepo['varnish']
+    }
+  } else {
+    yumrepo {'varnish':
+      descr    => 'varnish repository',
+      baseurl  => 'http://repo.varnish-cache.org/redhat/el5/x86_64/',
+      enabled  => 0,
+      gpgcheck => 0,
+    }
+
+    package {'varnish':
+      ensure  => $version,
+    }
+
   }
 
 }
