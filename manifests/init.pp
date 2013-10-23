c9a6aa2812457cffb39cc8708abf61fb52b15f59
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-10-23 09:42:46 +0200

    clean varnish init

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,30 +5,16 @@
 class varnish::init {
 
   $version = hiera('varnish-version', 'installed')
-  if $version == 'installed' {
-    yumrepo {'varnish':
-      descr    => 'varnish repository',
-      baseurl  => 'http://repo.varnish-cache.org/redhat/el5/x86_64/',
-      enabled  => 1,
-      gpgcheck => 0,
-    }
 
-    package {'varnish':
-      ensure  => 'installed',
-      require => Yumrepo['varnish']
-    }
-  } else {
-    yumrepo {'varnish':
-      descr    => 'varnish repository',
-      baseurl  => 'http://repo.varnish-cache.org/redhat/el5/x86_64/',
-      enabled  => 0,
-      gpgcheck => 0,
-    }
-
-    package {'varnish':
-      ensure  => $version,
-    }
-
+  # disable externe repo, we use a "cirb" repo
+  yumrepo {'varnish':
+    descr    => 'varnish repository',
+    baseurl  => 'http://repo.varnish-cache.org/redhat/el5/x86_64/',
+    enabled  => 0,
+    gpgcheck => 0,
   }
 
+  package {'varnish':
+      ensure  => $version,
+  }
 }
